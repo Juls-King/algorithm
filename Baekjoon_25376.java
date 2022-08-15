eb07f552d73c902c1b92bacd3947c0aefb7c70f2
KJS <turbostart@naver.com>
2022-08-15 19:27:59 +0900

    시간초과 해결 중

--- Baekjoon_25376.java
+++ Baekjoon_25376.java
@@ -9,107 +9,119 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Baekjoon_25376 {
+	static boolean visited[];
+	static int min_cnt = Integer.MAX_VALUE;
+	static int N;
+	static int[] switch_status;
+	static ArrayList<Integer>[] related_switch;
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
-		
-		int N = Integer.parseInt(br.readLine());
-		
-		int[] switch_status = new int[N + 1];
-		ArrayList<Integer>[] related_switch = new ArrayList[N + 1];
-		
+
+		N = Integer.parseInt(br.readLine());
+
+		visited = new boolean[N];
+
+		switch_status = new int[N + 1];
+		related_switch = new ArrayList[N + 1];
+
 		st = new StringTokenizer(br.readLine());
 		for (int i = 1; i <= N; i++) {
 			switch_status[i] = Integer.parseInt(st.nextToken());
-			
+
 			related_switch[i] = new ArrayList<Integer>();
 		}
-		
+
 		for (int i = 1; i <= N; i++) {
 			st = new StringTokenizer(br.readLine());
-			
+
 			int m = Integer.parseInt(st.nextToken());
-			
-			for(int j=1 ; j<=m ; j++) {
+
+			for (int j = 1; j <= m; j++) {
 				related_switch[i].add(Integer.parseInt(st.nextToken()));
 			}
 		}
-		
-		if(lightAll(N, switch_status) == true) {
+
+		if (lightAll()) {
 			System.out.println(0);
 			return;
 		}
-		
+
 		int result;
-		
-		result = solution(1, N, switch_status, related_switch);
 
-		System.out.println(result);
+		solution(1, 0);
+
+		if (min_cnt == Integer.MAX_VALUE) {
+			System.out.println(-1);
+		} else {
+			System.out.println(min_cnt);
+		}
+
 	}
-	
-	public static int solution(int i, int N, int[] switch_status, ArrayList<Integer>[] related_switch) {
-		Queue<Integer> q = new LinkedList<>();
-		int count = 0;
 
-		q.offer(i);
-//		visited[i - 1] = true;
+	public static void solution(int depth, int count) {
+		if (depth == N+1) { // 입력한 depth에 도달 시 출력
+			return;
+		}
 
-		while (!q.isEmpty()) {
-			int temp = q.poll();
-//			System.out.print(temp + " ");
+		for (int i = 1; i <= N; i++) { // 완전 탐색
+			if (visited[i - 1] == false && switch_status[i] == 0) {
+				visited[i - 1] = true; // 방문 처리
+//				System.out.println("(" + i + ")");
 
-			for (int j = 1; j <= N; j++) {
-				if (switch_status[j] == 0) {
-					q.offer(j);
-					switchOn(j, switch_status, related_switch);
-					count++;
-//					print(N, switch_status);
-//					visited[j] = true;
-				}
+				int[] temp = switch_status.clone();
 				
-				if(count > N) {
-					return -1;
+				switchOn(i);
+				count++;
+//				print();
+
+				if (lightAll()) {
+					if (min_cnt > count) {
+						min_cnt = count;
+					}
+
+					return;
 				}
+
+				solution(depth + 1, count);
+
+				visited[i - 1] = false; // 다음 depth 완료 후 방문여부를 초기화 해줘야 모든 경우에수 탐색 할 수 있다
+				
+				switch_status = temp.clone();
 			}
 		}
-		
-		if(count == 0) {
-			return -1;
-		}
-		
-		return count;
 	}
-	
-	public static boolean lightAll(int N, int[] switch_status) {
+
+	public static boolean lightAll() {
 		int count = 0;
-		
-		for(int i=1 ; i<=N ; i++) {
-			if(switch_status[i] == 1) {
+
+		for (int i = 1; i <= N; i++) {
+			if (switch_status[i] == 1) {
 				count++;
 			}
 		}
-		
-		if(count == N) {
+
+		if (count == N) {
 			return true;
 		}
-		
+
 		return false;
 	}
-	
-	public static void switchOn(int k, int[] switch_status, ArrayList<Integer>[] related_switch) {
+
+	public static void switchOn(int k) {
 		switch_status[k] = 1;
-		
+
 		int len = related_switch[k].size();
-		for(int i=0 ; i<len ; i++) {
+		for (int i = 0; i < len; i++) {
 			int val = related_switch[k].get(i);
-			
+
 			switch_status[val] = (switch_status[val] == 0) ? 1 : 0;
 		}
 	}
-	
-	public static void print(int N, int[] switch_status) {
-		for(int i=1 ; i<=N ; i++) {
+
+	public static void print() {
+		for (int i = 1; i <= N; i++) {
 			System.out.print(switch_status[i] + " ");
 		}
 		System.out.println();
