5544c0f66acab6f8f3fb92c49882ad055f97ca55
unknown <turbostart@naver.com>
2022-08-23 00:28:08 +0900

    시간초과 해결 안됨

--- Baekjoon_25376.java
+++ Baekjoon_25376.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Baekjoon_25376 {
@@ -33,23 +32,16 @@ public class Baekjoon_25376 {
 
 		st = new StringTokenizer(br.readLine());
 		for (int i = 1; i <= N; i++) {
-//			switch_status[i] = Integer.parseInt(st.nextToken());
 
 			related_switch[i] = new ArrayList<Integer>();
-			
-//			sb.append(switch_status[i]);
-			
+
 			sb.append(st.nextToken());
 			max_bit.append("1");
 		}
-		
+
 		sb_int = Integer.parseInt(sb.toString(), 2);
 		max_bit_int = Integer.parseInt(max_bit.toString(), 2);
-//		System.out.println("sb : " + sb);
-//		System.out.println("max_bit : " + max_bit);
-//		System.out.println("sb_int : " + sb_int);
-//		System.out.println("max_bit_int : " + max_bit_int);
-		
+
 		for (int i = 1; i <= N; i++) {
 			st = new StringTokenizer(br.readLine());
 
@@ -79,89 +71,50 @@ public class Baekjoon_25376 {
 		for (int i = 1; i <= N; i++) { // 완전 탐색
 
 			String test1 = Integer.toBinaryString(sb_int);
-			test1 = String.format("%0" + N + "d", Integer.parseInt(test1));
-//			System.out.println("test1 : " + test1);
-			
-//			System.out.println("test : " + test);
-//			System.out.println("test == '0' : " + (test == '0'));
-			
-			if (visited[i - 1] == false && test1.charAt(i-1) == '0') {
+//			test1 = String.format("%0" + N + "d", Long.parseLong(test1));
+
+			StringBuilder sb_ = new StringBuilder();
+			int test_len = N - test1.length();
+			for (int j = 0; j < test_len; j++) {
+				sb_.append("0");
+			}
+
+			test1 = sb_ + test1;
+
+			if (visited[i - 1] == false && test1.charAt(i - 1) == '0') {
 				visited[i - 1] = true; // 방문 처리
 
-//				int[] temp = switch_status.clone();
-//				StringBuilder sb_bak = sb;
 				int sb_bak = sb_int;
-				
-				switchOn_(i);
-				
-				count++;
-				
-//				print();
-//				System.out.println(Integer.toBinaryString(sb_int));
 
-//				if (lightAll()) {
+				switchOn_(i);
+				count++;
+
 				if (sb_int == max_bit_int) {
 					if (min_cnt > count) {
 						min_cnt = count;
 					}
-					
-//					System.out.println("lightAll : " + count);
+
 					return;
 				}
 
 				solution(count);
 
 				visited[i - 1] = false; // 다음 depth 완료 후 방문여부를 초기화 해줘야 모든 경우에수 탐색 할 수 있다
-				
-//				switch_status = temp.clone();
+
 				sb_int = sb_bak;
 			}
 		}
 	}
 
-	public static boolean lightAll() {
-		int count = 0;
-
-		for (int i = 1; i <= N; i++) {
-			if (switch_status[i] == 1) {
-				count++;
-			}
-		}
-
-		if (count == N) {
-			return true;
-		}
-
-		return false;
-	}
-
-//	public static void switchOn(int k) {
-//		switch_status[k] = 1;
-//
-//		int len = related_switch[k].size();
-//		for (int i = 0; i < len; i++) {
-//			int val = related_switch[k].get(i);
-//
-//			switch_status[val] = (switch_status[val] == 0) ? 1 : 0;
-//		}
-//	}
-	
 	public static void switchOn_(int k) {
-		sb_int ^= (1 << (N-k));
+		sb_int ^= (1 << (N - k));
 
 		int len = related_switch[k].size();
-		
+
 		for (int i = 0; i < len; i++) {
 			int val = related_switch[k].get(i);
-			
-			sb_int ^= (1 << (N-val));
+
+			sb_int ^= (1 << (N - val));
 		}
 	}
-
-//	public static void print() {
-//		for (int i = 1; i <= N; i++) {
-//			System.out.print(switch_status[i] + " ");
-//		}
-//		System.out.println();
-//	}
 }
