8ff0c0ee414a7041979ccfa3e6fe3f05deb3fb27
unknown <turbostart@naver.com>
2022-10-04 23:39:23 +0900

    킹과폰 입력받는 중

---
+++ Baekjoon_9114.java
@@ -0,0 +1,25 @@
+package algorithm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Baekjoon_9114 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+		
+		char[][][] map = new char[N][8][8];
+		
+		for(int i=0 ; i<N ; i++) {
+			for(int j=0 ; j<8 ; j++) {
+				for(int k=0 ; k<8 ; k++) {
+					
+				}
+			}
+		}
+	}
+
+}
