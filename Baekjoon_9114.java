bb1e9e60d21b4f7f0514c99590b08624bc0c85b1
KJS <turbostart@naver.com>
2022-10-05 17:59:07 +0900

    킹과 폰 고군분투 중

--- Baekjoon_9114.java
+++ Baekjoon_9114.java
@@ -3,23 +3,78 @@ package algorithm;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Baekjoon_9114 {
+	static int N;
+	static char[][][] map;
+	static int[][][] pos;
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
 
-		int N = Integer.parseInt(br.readLine());
+		N = Integer.parseInt(br.readLine());
 		
-		char[][][] map = new char[N][8][8];
+		map = new char[N][8][8];
+		pos = new int[N][2][2];
 		
 		for(int i=0 ; i<N ; i++) {
 			for(int j=0 ; j<8 ; j++) {
+				String str = br.readLine();
+				
 				for(int k=0 ; k<8 ; k++) {
-					
+					map[i][j][k] = str.charAt(k);
 				}
 			}
+			
+			for(int j=0 ; j<2 ; j++) {
+				st = new StringTokenizer(br.readLine());
+				
+				pos[i][j][0] = Integer.parseInt(st.nextToken());
+				pos[i][j][1] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		for(int i=0 ; i<5 ; i++) {
+			
+		}
+		
+		print(0);
+		
+		
+		
+	}
+	
+	public static void print(int i) {
+		for(int j=0 ; j<8 ; j++) {
+			for(int k=0 ; k<8 ; k++) {
+				System.out.print(map[i][j][k]);
+			}
+			System.out.println();
+		}
+		System.out.println();
+	}
+	
+	public static void print() {
+		for(int i=0 ; i<N ; i++) {
+			for(int j=0 ; j<8 ; j++) {
+				for(int k=0 ; k<8 ; k++) {
+					System.out.print(map[i][j][k]);
+				}
+				System.out.println();
+			}
+			System.out.println();
+		}
+		
+		for(int i=0 ; i<N ; i++) {
+			for(int j=0 ; j<2 ; j++) {
+				for(int k=0 ; k<2 ; k++) {
+					System.out.print(pos[i][j][k] + " ");
+				}
+				System.out.println();
+			}
+			System.out.println();
 		}
 	}
-
 }
