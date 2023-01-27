d80ab66435efd78512739603a5a84dced5e57aa4
Juls-King <97081505+Juls-King@users.noreply.github.com>
2023-01-28 00:03:44 +0900

    [Silver III] Title: 단어 뒤집기 2, Time: 344 ms, Memory: 23764 KB -BaekjoonHub

--- 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.java
+++ 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.java
@@ -1,12 +1,15 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Stack;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		String str = br.readLine();
 		String[] strAry = str.split("");
 		Stack<String> stack = new Stack<String>();
@@ -16,23 +19,28 @@ public class Main {
 		for (String s : strAry) {
 			if (s.equals(">")) {
 				flag = false;
-				System.out.print(s);
+//				System.out.print(s);
+				bw.write(s);
 				continue;
 			} else if (s.equals("<") || flag == true) {
 				while (!stack.isEmpty()) {
-					System.out.print(stack.pop());
+//					System.out.print(stack.pop());
+					bw.write(stack.pop());
 				}
 
 				flag = true;
-				System.out.print(s);
+//				System.out.print(s);
+				bw.write(s);
 				continue;
 			}
 
 			if (s.equals(" ")) {
 				while (!stack.isEmpty()) {
-					System.out.print(stack.pop());
+//					System.out.print(stack.pop());
+					bw.write(stack.pop());
 				}
-				System.out.print(" ");
+//				System.out.print(" ");
+				bw.write(" ");
 				continue;
 			}
 
@@ -40,7 +48,11 @@ public class Main {
 		}
 
 		while (!stack.isEmpty()) {
-			System.out.print(stack.pop());
+//			System.out.print(stack.pop());
+			bw.write(stack.pop());
 		}
+		
+		bw.flush();
+		bw.close();
 	}
 }
