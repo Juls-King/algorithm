ffdea33e4683b2d9560aa8c5a94aab1cf3ce897f
unknown <turbostart@naver.com>
2023-01-31 23:56:55 +0900

    쇠막대기 쉽지않네

--- B10799.java
+++ B10799.java
@@ -5,14 +5,23 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
 import java.util.Stack;
 
 public class B10799 {
+	public static class Stick {
+		public int index;
+		
+		public Stick(int index) {
+			this.index = index;
+		}
+	}
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		Stack<String> stack = new Stack<String>();
+		ArrayList<Integer> alist = new ArrayList<Integer>();
 		
 		String input = br.readLine();
 		
@@ -21,14 +30,23 @@ public class B10799 {
 		int count = 0;
 		
 		for(String s : strAry) {
-			if(!stack.isEmpty()) {
-				if(stack.peek().equals("(") && s.equals(")")) {
-					count++;
+			switch(s) {
+			case "(":
+				stack.push(s);
+				
+				
+				
+				alist.add(0);
+				break;
+			case ")":
+				if(stack.peek().equals("(")) {
+					stack.pop();
 				}
+				break;
 			}
-			
-			stack.push(s);
 		}
-		System.out.println(count);
+		
+
+
 	}
 }
