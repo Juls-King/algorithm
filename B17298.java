b441c14508bd709e0e5d6bd2daf1474696f49fad
unknown <turbostart@naver.com>
2023-02-12 23:44:23 +0900

    내일은 꼭 푼다

--- B17298.java
+++ B17298.java
@@ -3,6 +3,8 @@ package algorithm;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
 
 public class B17298 {
 
@@ -11,10 +13,46 @@ public class B17298 {
 		
 		int N = Integer.parseInt(br.readLine());
 		
+		int[] ary = new int[N];
+		ary[N-1] = -1;
+		
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		StringBuilder sb = new StringBuilder();
+		
+		Stack<Integer> stack = new Stack<Integer>();
+		Stack<Integer> stack2 = new Stack<Integer>();
+		
 		for(int i=0 ; i<N ; i++) {
-			for(int j=i ; j<N ; j++) {
-				
+			int num = Integer.parseInt(st.nextToken());
+			
+			if(i == 0) {
+				stack.push(i);
+				stack2.push(num);
+				continue;
 			}
+			
+			if(stack.isEmpty() == true) {
+				stack.push(i);
+				stack2.push(num);
+				continue;
+			}
+			else if(stack2.peek() > num) {
+				stack.push(i);
+				stack2.push(num);
+			}
+			else if (stack.peek() < num) {
+				while(!stack.isEmpty()) {
+					ary[stack.pop()] = num;
+					stack2.pop();
+				}
+			}
+			
+			stack.push(i);
+			stack2.push(num);
+		}
+		
+		for(int i=0 ; i<N ; i++) {
+			System.out.print(ary[i] + " ");
 		}
 	}
 }
