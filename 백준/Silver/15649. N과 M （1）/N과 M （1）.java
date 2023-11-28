87a0166b33a70aec7fdc89ec936f5b9ec1c64eb2
Juls-King <97081505+Juls-King@users.noreply.github.com>
2023-11-28 23:13:44 +0900

    [Silver III] Title: N과 M (1), Time: 224 ms, Memory: 21636 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,51 +1,42 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-	static Boolean[] visited;	// 방문여부 확인 배열
-	static int N;
-	static int M;
-
+	static int N, M;
+	static char[] arr;
+	static boolean[] ck;
+	static StringBuilder sb = new StringBuilder();
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-		String s = br.readLine();
-
-		StringTokenizer st = new StringTokenizer(s);
-
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-
-		visited = new Boolean[N + 1];
-		Arrays.fill(visited, false);
 		
-		int[] ary = new int[M];
-		
-		dfs(0, ary);    // 함수 호출
+		arr = new char[2 * M];
+		ck = new boolean[N + 1];
+		back(0);
+		System.out.print(sb);
 	}
-
-	public static void dfs(int depth, int[] ary) {
-		if(depth == M) {	// 입력한 depth에 도달 시 출력
-			for(int a=0 ; a<M ; a++) {
-				System.out.print(ary[a] + " ");
-			}
-			System.out.println();
-			
-			return;
+	
+	static void back(int idx) {
+		if (idx >= M) {
+			arr[2 * M - 1] = '\n';
+			sb.append(arr);
+			return ;
 		}
 		
-		for(int j=1 ; j<=N ; j++) { // 완전 탐색
-			if(visited[j] == false) {
-				visited[j] = true;	// 방문 처리
-				
-				ary[depth] = j;		// 현재의 index값을 현재의 depth 자리에 대입
-				
-				dfs(depth+1, ary);	// 재귀호출, 다음 depth로 들어간다
-				
-				visited[j] = false;	// 다음 depth 완료 후 방문여부를 초기화 해줘야 모든 경우에수 탐색 할 수 있다
+		int i = 0;
+		for (i = 1; i <= N; i++) {
+			if (!ck[i]) {
+				ck[i] = true;
+				arr[2 * idx] = (char)(i + '0');
+				arr[2 * idx + 1] = ' ';
+				back(idx + 1);
+				ck[i] = false;
 			}
 		}
 	}
