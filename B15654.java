f8d4cac17fcd8ceba1985f3e31a17f07d09e45a5
unknown <turbostart@naver.com>
2023-12-07 23:02:56 +0900

    여전히 트라이 중

--- B15654.java
+++ B15654.java
@@ -10,7 +10,7 @@ public class B15654 {
 
 	static boolean[] visited;
 	static int N, M;
-	static String[] ary;
+	static int[] ary;
 	static StringBuilder sb = new StringBuilder();
 	static int[] nums;
 
@@ -22,7 +22,7 @@ public class B15654 {
 		M = Integer.parseInt(st.nextToken());
 
 		visited = new boolean[N + 1];
-		ary = new String[M * 2];
+		ary = new int[M * 2];
 		nums = new int[N + 1];
 		
 		st = new StringTokenizer(br.readLine());
@@ -41,22 +41,20 @@ public class B15654 {
 
 	public static void dfs(int depth) {
 		if (depth == M) {
-			ary[2 * M - 1] = "\n";
+//			ary[2 * M - 1] = "\n";
 			sb.append(Arrays.toString(ary));
+			sb.append("\n");
 			return;
 		}
 
 		int i = 0;
 		for (i = 1; i <= N; i++) {
 			if (visited[i] == false) {
-//				if (depth > 0 && i <= (int) ary[2 * depth - 2] - '0') {
-//					continue;
-//				}
-
 				visited[i] = true;
 
-				ary[2 * depth] = Integer.toString(nums[i]);
-				ary[2 * depth + 1] = "";
+//				ary[2 * depth] = Integer.toString(nums[i]);
+//				ary[2 * depth + 1] = "";
+				ary[2 * depth] = nums[i];
 
 				dfs(depth + 1);
 
